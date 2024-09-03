aef9f48af6f9d5be79271d4a610326a310d49ef5
moulineE <elmahdi.mouline@live.fr>
2024-09-03 21:54:16 +0100

    Update views.py to filter unpublished posts for anonymous users
    - Modified `BlogHome` view to override `get_queryset` method
    - Added logic to filter out unpublished posts for anonymous users

--- src/posts/views.py
+++ src/posts/views.py
@@ -9,3 +9,13 @@ class BlogHome(ListView):
     """Home page view."""
     model = BlogPost
     context_object_name = "posts"
+
+    def get_queryset(self):
+        """
+        Override get_queryset method to filter unpublished posts
+        for anonymous users.
+        """
+        queryset = super().get_queryset()
+        if self.request.user.is_authenticated:
+            return queryset
+        return queryset.filter(published=True)
