6b3e16fc832044e65a998a7190771e01ad67b82a
moulineE <elmahdi.mouline@live.fr>
2024-09-03 20:39:10 +0100

    Blogpost model fix

--- src/posts/models.py
+++ src/posts/models.py
@@ -11,7 +11,7 @@ class BlogPost(models.Model):
     slug = models.SlugField(max_length=255, unique=True, blank=True)
     author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
     last_update = models.DateTimeField(auto_now=True)
-    created_on = models.DateField(blang=True, null=True)
+    created_on = models.DateField(blank=True, null=True)
     published = models.BooleanField(default=False, verbose_name="Publié")
     content = models.TextField(blank=True, verbose_name="Contenu")
 
