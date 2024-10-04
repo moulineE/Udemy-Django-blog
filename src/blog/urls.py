7a0d2df1311bfcde7abde9bbffb9b4726c9c7a2b
moulineE <elmahdi.mouline@live.fr>
2024-10-04 12:13:27 +0100

    Update models.py, settings.py, urls.py and blogpost_detail.html to add thumbnail image to blog model and serve it

--- src/blog/urls.py
+++ src/blog/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.conf.urls.static import static
 from django.urls import path, include
+from blog import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('blog/', include('posts.urls'))
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
